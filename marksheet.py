d0b6d1c86643d8d411f42da0d4bb2ba1fe70baed
AakashDayma01 <daymaaakash334@gmail.com>
2026-06-15 13:25:40 +0530

    added dynami school name

--- marksheet.py
+++ marksheet.py
@@ -6,14 +6,12 @@ class Marksheet:
 	scholar_number = random.randrange(1111111, 9999999)
 
 	# String with scholar number and state board name correctly formatted
-	state_board = f"{'S. No.':>20} {scholar_number:<30}CENTRAL BOARD OF SECONDARY EDUCATION"
+	state_board = f"{'|':>11}{'S. No.':>9} {scholar_number:<32}{'CENTRAL BOARD OF SECONDARY EDUCATION'} {'|':>40}"
 
 	# String with Exam Type 
-	exam_type = f"{'ALL INDIA':>52} {'SENIOR SCHOOL CERTIFICATE EXAMINATION,2026'} {'SCH-' + str(random.randrange(111111111, 999999999)) :>30} "
+	exam_type = f"{'|':>11}{'ALL INDIA':>43} {'SENIOR SCHOOL CERTIFICATE EXAMINATION,2026'} {'SCH-' + str(random.randrange(111111111, 999999999)) :>28} {'|':>3}"
 
 	# String with School name
-	school_name = f"{'Govt. Model Higher Secondary School Manasa (M. P.)':>95} "
-
 
 	# List Of Failed Subjects
 
@@ -26,25 +24,19 @@ class Marksheet:
 			self.Absent_subject += 1
 			self.failed_subjects.append(subject)
 			return "AB"
-		elif not(str(marks).isnumeric()) or str(marks).isalpha():
-			maths = self.auth_only_theory_sub(input(f"Enter valid Marks for subject {subject}:  "), subject)
-		marks = int(marks)		
-		if int(marks) <= 100 and int(marks) >= 0:
-			return (marks := int(marks))
+		elif str(marks).isnumeric() and int(marks) <= 100 and int(marks) >= 0:
+			return int(marks)
 		else:
-			print(f"Marks of {subject} must be less than equal to 100")
-			maths = self.auth_only_theory_sub(input(f"Enter marks for subject {subject}:  "), subject)
+			return self.auth_only_theory_sub(input(f"Marks of {subject} must be less than equal to 100:  "), subject)
 
 	# Authenticate marks of Practical subjects
 	def auth_practical_subject(self, marks, subject):
-		if not(str(marks).isnumeric()) or str(marks).isalpha():
-			print(f"Enter valid marks for subject {subject}")
-			return self.auth_practical_subject(input(f"Enter marks for subject {subject}: "), subject)	
+		if not(str(marks).isnumeric()):
+			return self.auth_practical_subject(input(f"Enter valid marks for subject {subject}: "), subject)	
 		elif str(marks).isnumeric() and (int(marks) <= 20 and int(marks) >= 0):
 			return (marks := int(marks))
 		else:
-			print(f"Marks of {subject} must be less than equal to 20")
-			return self.auth_practical_subject(input(f"Enter marks for subject {subject}: "), subject)
+			return self.auth_practical_subject(input(f"Marks of {subject} must be less than equal to 20: "), subject)
 
 	# Authenticate marks of subjects that also have practical Exams
 	def auth_sub_with_pr(self, marks, subject):
@@ -52,37 +44,51 @@ class Marksheet:
 			self.Absent_subject += 1
 			self.failed_subjects.append(subject)
 			return "AB"
-		elif not(str(marks).isnumeric()) or str(marks).isalpha():
+		elif not(str(marks).isnumeric()):
 			return self.auth_sub_with_pr(input(f"Enter valid marks for subject {subject}: "),subject)	
 
 		marks = int(marks)		
 		if str(marks).isnumeric() and (int(marks)) <= 80 and int(marks) >= 0:
 			return (marks := int(marks))
 		else:
-			print(f" Marks of {subject} must be less than equal to 80")
-			return self.auth_sub_with_pr(input(f"Enter marks for subject {subject}: "),subject)
+			return self.auth_sub_with_pr(input(f"Marks of {subject} must be less than equal to 80: "),subject)
 
 	# Authenticate Roll Number
 	def auth_roll_num(self, r_number):
 		if r_number.isnumeric() and len(r_number) == 7:
 			return r_number
 		else:
-			print("Enter a valid Roll Number")
-			return self.auth_roll_num(input("Enter Roll Number: "))
+			if len(r_number) != 7:
+				return self.auth_roll_num(input("Length of roll number must be 7: "))
+			else:
+				return self.auth_roll_num(input("Please enter only numbers don't use special letters and spellings: "))
+
+
+	# Check whether any sumbers present in the name or not
+	def check_name(self, name):
+		special_letters = ('#', "@", "!", "_", "$", "%", "&", "," "^", "*", "(", ")", "{" , "}", "\\" , "|", "?", ":", ";", ">", "<", "/")
+		for index, letter in enumerate(special_letters):
+			if (str(index) in name) or (letter in name):
+				return self.check_name(input("Please don't use special characters and numbers in name: "))
+		else:
+			return name
 
 	# Constructor method 	
 	def __init__(self):	
 		# User Name
-		self.name = input("Enter Name: ")
+		self.name = self.check_name(input("Enter Name: "))
 		self.roll_number = self.auth_roll_num(input("Enter roll number: "))
+		self.school_name = self.check_name(input("Enter school name: "))
+		self.father_name = self.check_name(input("Enter father name: "))
+		self.mother_name = self.check_name(input("Enter mother name:"))
 
 		# Only THeory Subjects	
-		self.physics = self.auth_sub_with_pr(input("Enter marks for subject Physics Enter 'AB' id Absent: "),subject = "Physics")
+		self.physics = self.auth_sub_with_pr(input("Enter marks for subject Physics Enter 'AB' if Absent: "),subject = "Physics")
 		self.chemistry = self.auth_sub_with_pr(input("Enter marks for subject Chemistry Enter \"AB\" id Absent: "), subject = "Chemistry")
 
 		# Theory Subject That Have Practical Exams
-		self.maths = self.auth_only_theory_sub(input("Enter marks for subject Maths Enter \"AB\" id Absent:  "), subject = "Maths")
-		self.hindi = self.auth_only_theory_sub(input("Enter marks for subject Hindi Enter \"AB\" id Absent: "), subject = "Hindi")
+		self.maths = self.auth_only_theory_sub(input("Enter marks for subject Maths Enter \"AB\" if Absent:  "), subject = "Maths")
+		self.hindi = self.auth_only_theory_sub(input("Enter marks for subject Hindi Enter \"AB\" if Absent: "), subject = "Hindi")
 		self.english = self.auth_only_theory_sub(input("Enter marks for subject English Enter \"AB\" id Absent: "), subject = "English")
 
 		# Practical Subjects
@@ -111,62 +117,73 @@ class Marksheet:
 
 
 	def markshit_top_content(self):
-		self.name_string = f"{'Name:':>19} {self.name:<85} Roll Number: {self.roll_number}"
+		self.name_string = f"{'|':>11}{'Name:':>8} {self.name:<85} Roll Number: {self.roll_number} {'|':>3}"
 		print("-"*150 + "\n")
-		print(f"{ '-'*113:>127}")
+		print(f"{ '-'*120:>130}")
 		print(self.state_board)
 		print(self.exam_type)
-		print(self.school_name, "\n\n")
+		self.school_name = f"{'| ':>12} {self.school_name:^116}|"
+		print(self.school_name)
+		print(f"{'|':>11}{'|':>119}")
+		print(f"{'|':>11}{'|':>119}")
 		print(self.name_string)
-		print(f"{ '-'*113:>127}")
+		print(f"{'|':>11}{'Father name: ':>16}{self.father_name:<85}{'|':>18}")
+		print(f"{'|':>11}{'Mother name: ':>16}{self.mother_name:<85}{'|':>18}")
+		print(f"{'|':>11}{ '-'*113:>116}{'|':>3}")
 
 		# Printing Headings
-		print(f"{'|':>15}" , f"{"|":>6}" , f"{'|':>34}" , end = " ")
-		print( f"{'MARKS OBTAINED':^67}" , f"{'|'}")
-		print(f"{'|':>15}" , f"{'|':>6}" , f"{'|':>34}" , f"{'-' * 67}" , "|")
+		print(f"{'|':>11}{'|':>4}" , f"{"|":>6}" , f"{'|':>34}" , end = " ")
+		print( f"{'MARKS OBTAINED':^67}" , f"{'|'} {'|':>2}")
+		print(f"{'|':>11}{'|':>4}" , f"{'|':>6}" , f"{'|':>34}" , f"{'-' * 67}" , "|",f"{'|':>2}")
 
-		print(f"{'| CODE |':>22}" , f"{"SUBJECT":^32}","|" , f"{'TH':^8}","|" , f"{'PR':^8}",end = " ")
-		print("|" , f"{'TOTAL':^8}" , "|" , f"{'TOTAL IN WORDS':^8}" , "|" , f"{'POSITIONAL GRADE':^8} ", "|")
+		print(f"{'|':>11}{'| CODE |':>11}" , f"{"SUBJECT":^32}","|" , f"{'TH':^8}","|" , f"{'PR':^8}",end = " ")
+		print("|" , f"{'TOTAL':^8}" , "|" , f"{'TOTAL IN WORDS':^8}" , "|" , f"{'POSITIONAL GRADE':^8} ", "|",f"{'|':>2}")
 
-		print(f"{'|':>15}" , f"{"|":>6}" , f"{'|':>34}" , f"{'|':>10}",end = " ")
-		print(f"{'|':>10}" , f"{'|':>10}" , f"{'|':>16} ", f"{'|':>18}")
-		print(f"{ '-'*113:>127}")
+		print(f"{'|':>11}{'|':>4}" , f"{"|":>6}" , f"{'|':>34}" , f"{'|':>10}",end = " ")
+		print(f"{'|':>10}" , f"{'|':>10}" , f"{'|':>16} ", f"{'|':>18}{'|':>3}")
+		print(f"{'|':>11}{ '-'*113:>116}{'|':>3}")
 
 
 		# Printing Subjects with marks
 		p = inflect.engine()
 		
 		# For Subjct Maths
-		print(f"{'|':>15}",f"{'041 |':^8}",f"{"MATHEMATICS":^30}","|",f"{str(self.maths):^8}","|",f"{'XX':^8}","|",f"{str(self.maths):^8}","|" ,end ="")
-		print(f"{(str(p.number_to_words(89)).upper()):^15}" , "|" , f"{str(self.gen_only_th_grade(self.maths, "Mathematics")):^17}" , "|")
+		print(f"{'|':>11}{'|':>4}",f"{'041 |':^8}",f"{"MATHEMATICS":^30}","|",f"{str(self.maths):^8}","|",f"{'XX':^8}","|",f"{str(self.maths):^8}","|" ,end ="")
+		print(f"{(str(p.number_to_words(self.maths)).upper()):^15}" , "|" , f"{str(self.gen_only_th_grade(self.maths, "Mathematics")):^17}" , "|",f"{'|':>2}")
 
 		# For Subject Physics
 		self.physics_total = (self.physics + self.physics_pr) if(str(self.physics).lower()!="ab") else self.physics_pr
-		print(f"{'|':>15}",f"{'042 |':^8}",f"{"PHYSICS":^30}","|",f"{str(self.physics):^8}","|",f"{str(self.physics_pr):^8}","|",f"{str(self.physics_total):^8}","|" ,end ="")
-		print(f"{(str(p.number_to_words(self.physics_total)).upper()):^15}" , "|" , f"{self.gen_only_th_grade(self.physics_total, "Physics"):^17}" , "|")
+		print(f"{'|':>11}{'|':>4}",f"{'042 |':^8}",f"{"PHYSICS":^30}","|",f"{str(self.physics):^8}","|",f"{str(self.physics_pr):^8}","|",f"{str(self.physics_total):^8}","|" ,end ="")
+		print(f"{(str(p.number_to_words(self.physics_total)).upper()):^15}","|", f"{self.gen_only_th_grade(self.physics_total,"Physics"):^17}","|",f"{'|':>2}")
 
 		# For Subject Chemistry
 		self.chemistry_total = (self.chemistry + self.chemistry_pr) if(str(self.chemistry).lower()!="ab") else self.chemistry_pr 
-		print(f"{'|':>15}",f"{'043 |':^8}",f"{"CHEMISTRY":^30}","|",f"{str(self.chemistry):^8}","|",f"{str(self.chemistry_pr):^8}","|",f"{str(self.chemistry_total):^8}","|",end ="")
-		print(f"{(str(p.number_to_words(self.chemistry_total)).upper()):^15}" , "|" , f"{self.gen_only_th_grade(self.chemistry_total, "Chemistry"):^17}" , "|")
+		print(f"{'|':>11}{'|':>4}",f"{'043 |':^8}",f"{"CHEMISTRY":^30}","|",f"{str(self.chemistry):^8}","|",f"{str(self.chemistry_pr):^8}","|",f"{str(self.chemistry_total):^8}","|",end ="")
+		print(f"{(str(p.number_to_words(self.chemistry_total)).upper()):^15}","|",f"{self.gen_only_th_grade(self.chemistry_total,"Chemistry"):^17}","|",f"{'|':>2}")
 
 		# For Subject English
-		print(f"{'|':>15}" , f"{'301 |':^8}" , f"{"ENGLISH":^30}" , "|" , f"{89:^8}" , "|" , f"{'XX':^8}" , "|" , f"{89:^8}" , "|" ,end ="")
-		print(f"{(str(p.number_to_words(self.english)).upper()):^15}" , "|" , f"{self.gen_only_th_grade(99, "English"):^17}" , "|")
+		print(f"{'|':>11}{'|':>4}",f"{'301 |':^8}",f"{"ENGLISH":^30}","|",f"{self.english:^8}","|",f"{'XX':^8}","|",f"{self.english:^8}","|" ,end ="")
+		print(f"{(str(p.number_to_words(self.english)).upper()):^15}","|",f"{self.gen_only_th_grade(self.english, "English"):^17}","|",f"{'|':>2}")
 
 		# For Subject Hindi
-		print(f"{'|':>15}" , f"{'302 |':^8}" , f"{"HINDI":^30}" , "|" , f"{89:^8}" , "|" , f"{'XX':^8}" , "|" , f"{89:^8}" , "|" ,end ="")
-		print(f"{(str(p.number_to_words(self.english)).upper()):^15}" , "|" , f"{self.gen_only_th_grade(99, "Hindi"):^17}" , "|")
+		print(f"{'|':>11}{'|':>4}" , f"{'302 |':^8}" , f"{"HINDI":^30}" , "|" , f"{self.hindi:^8}" , "|" , f"{'XX':^8}" , "|" , f"{self.hindi:^8}" , "|" ,end ="")
+		print(f"{(str(p.number_to_words(self.hindi)).upper()):^15}" , "|" , f"{self.gen_only_th_grade(self.hindi, "Hindi"):^17}" , "|",f"{'|':>2}")
 
-		print(f"{ '-'*113:>127}" , "\n\n")
+		print(f"{'|':>11}{ '-'*113:>116}{'|':>3}")
+		print(f"{'|':>11}{'|':>119}")
+		print(f"{'|':>11}{'|':>119}")
 
 
 		# PRinting Result
-		print(f"{'RESULT: ':>70}", f"{'PASS' if len(self.failed_subjects)<=2 else "FAIL"}")
+		print(f"{'|':>11}{'RESULT: ':>59}", f"{'PASS' if len(self.failed_subjects)<=2 else "FAIL"}{'|':>55}")
+		print(f"{'|':>11}{'|':>119}")
+		print(f"{'|':>11}{'|':>119}")
+		print(f"{'|':>11}{ '-'*118:>112}{'|'}")
 
-		print(f"{'Abbriviations':>27}")
-		print(f"{'AB: Absent in the Subject: ':>41}", f"{self.Absent_subject}")
-		print(f"{'FT: Fail in Theory Subject':>40}" , f"{len(self.failed_subjects)}")
+		print(f"{'|':>11}{'|':>119}")
+		print(f"{'|':>11}{'Abbriviations':>16}{'|':>103}")
+		print(f"{'|':>11}{'AB: Absent in the Subject: ':>30}", f"{self.Absent_subject}{'|':>87}")
+		print(f"{'|':>11}{'FT: Fail in Theory Subject':>29}" , f"{len(self.failed_subjects)}{'|':>88}")
 
 
 		self.failed_sub = ""
@@ -176,8 +193,10 @@ class Marksheet:
 			elif len(self.failed_subjects) == 2:
 				self.failed_sub= f" \"{self.failed_subjects[0]}\""
 				self.failed_sub += f" , \" {self.failed_subjects[1]}\""
-		print(f"{'*: Appeared in compartmental examination: ':>56}" , f"{self.failed_sub if (self.failed_sub != "") else 0}")
-		print("Delhi")
+		print(f"{'|':>11}{'*: Appeared in compartmental examination: ':>45}" , f"{self.failed_sub if self.failed_sub else " 0 " + ' ':<71} |")
+		print(f"{'|':>11}{'|':>119}")
+
+		print(f"{'|':>11}{ '-'*118:>112}{'|'}")
 
 		
 m1 = Marksheet()
